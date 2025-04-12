3db4c9d7eef150e5b9d054b3121a0407ac5189f1
Thomas Jacob <147674507+tj06boy@users.noreply.github.com>
2025-04-13 00:01:16 +0530

    Add files via upload

---
+++ 08_multiplication_table.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d",&n);
+
+    printf("Multiplication table of %d is:\n",n);
+
+    for (int i= 1; i<=10; i++)
+    {
+    printf("%d x %d = %d\n", n, i, n*i);
+    }
+
+    return 0;
+}
