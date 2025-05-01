18c5828b48c51064758a188df2b3075c64f6d4b7
Thomas Jacob <147674507+tj06boy@users.noreply.github.com>
2025-05-01 09:15:11 -0400

    Add files via upload

---
+++ 16_lcm.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int gcd(int a, int b)
+{
+    while(b!=0)
+    {
+        int temp= b;
+        b= a%b;
+        a=temp;        
+    }
+    return a;
+}
+
+int main()
+{
+    int num1,num2;
+    
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    int lcm= (num1*num2)/ gcd(num1,num2);
+
+    printf("LCM of %d and %d is: %d\n", num1, num2, lcm);
+    return 0;
+}
